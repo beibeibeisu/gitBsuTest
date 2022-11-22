a56c32e1c088c28ea3bd799c679bf34b7975402f
beisu <beisu@qq.com>
2022-11-22 22:02:25 +0800

    secondCommit

--- src/main/java/MyGitTest.java
+++ src/main/java/MyGitTest.java
@@ -2,5 +2,6 @@ public class MyGitTest {
     public static void main(String[] args) {
         System.out.println("firstCommit");
         System.out.println("hot-fix-firstCommit");
+        System.out.println("secondCommit");
     }
 }
